bea3651d19bf4ad4b4cb3288023b07376cf980a2
hfguan <hfguan@thoughtworks.com>
2020-05-23 14:17:50 +0800

    add observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,6 +24,6 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes() {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 }
