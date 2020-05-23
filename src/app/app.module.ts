13be6356b905c699bf469ec7aa5611ad0254747a
hfguan <hfguan@thoughtworks.com>
2020-05-23 14:49:58 +0800

    add dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import {HeroService} from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
